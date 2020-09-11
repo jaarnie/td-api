f6fbe30a4fd8b70a3ecde268201c071316945f51
John Arnold <johnadamarnold@gmail.com>
2020-09-11 08:42:05 +0100

    fixes

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,5 +1,15 @@
 class EntrySerializer < ActiveModel::Serializer
-  attributes :id, :content_title, :content, :mood, :user_entry_datetime, :user, :therapist_id
+  attributes :id, :content_title, :content, :mood, :user_entry_datetime, :therapist_id, :user_full_name
   belongs_to :user
   belongs_to :therapist
+
+  # def user
+  #   require 'pry'; binding.pry
+  # end
+
+  def user_full_name
+    user = object.user
+    user.first_name + " " + user.last_name
+  end
+
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,17 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name, :entries, :therapists
+  attributes :id, :email, :first_name, :last_name, :therapists, :entries
 
   # has_many :therapists, through: :entries
 
   def entries 
-    object.entries
+    entries = []
+    object.entries.each do |e|
+      attributes = e.attributes
+      full_name = object.first_name + " " + object.last_name
+      attributes[:username] = full_name
+      entries << attributes
+    end
+    entries
   end
 
   def therapists
